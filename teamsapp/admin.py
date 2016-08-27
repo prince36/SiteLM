d1eed03ed3444dde80b7909637b6ec3e0426d4f5
Ja <prince36@onet.pl>
2016-08-28 01:13:16 +0200

    first commit

--- teamsapp/admin.py
+++ teamsapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import iteam
 
-# Register your models here.
+admin.site.register(iteam)
--- teamsapp/views.py
+++ teamsapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def team_list(request):
+    return render(request, 'teamsapp/team_list.html', {})
